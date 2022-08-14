d53fe39ac31106f4715c490ce9520ab0797d764c
unknown <shivanshuaggarwal882@gmail.com>
2022-08-14 18:00:41 +0530

    Updates

---
+++ precache-manifest.1aa95b01406c70dd6568e3b8dd280302.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65f9f5d08abe21daf84c64b3982dae6",
+    "url": "/keepin/index.html"
+  },
+  {
+    "revision": "d96ffdeec8cb0fce6314",
+    "url": "/keepin/static/js/2.0602d0bb.chunk.js"
+  },
+  {
+    "revision": "2621983bfd56c8190f5d",
+    "url": "/keepin/static/js/main.a4c29f84.chunk.js"
+  },
+  {
+    "revision": "2f245b71226f978c0b5c",
+    "url": "/keepin/static/js/runtime-main.aeba03eb.js"
+  }
+]);
