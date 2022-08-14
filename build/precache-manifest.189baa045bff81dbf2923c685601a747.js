2b24ad52c84f9f978ee64b50bdbde8af139c9304
unknown <shivanshuaggarwal882@gmail.com>
2022-08-14 17:18:28 +0530

    Configure React app for deployment to GitHub Pages

---
+++ build/precache-manifest.189baa045bff81dbf2923c685601a747.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4b1559363ec4e055b7ba235bf8bb2d",
+    "url": "/keepin/index.html"
+  },
+  {
+    "revision": "d96ffdeec8cb0fce6314",
+    "url": "/keepin/static/js/2.0602d0bb.chunk.js"
+  },
+  {
+    "revision": "2621983bfd56c8190f5d",
+    "url": "/keepin/static/js/main.a4c29f84.chunk.js"
+  },
+  {
+    "revision": "2f245b71226f978c0b5c",
+    "url": "/keepin/static/js/runtime-main.aeba03eb.js"
+  }
+]);
